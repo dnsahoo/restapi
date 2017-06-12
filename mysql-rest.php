94403721a3c86c70eb092225813a1dc6d5515f70
dnsahoo <dnsahoophp@gmail.com>
2017-06-12 15:45:03 +0530

    Create mysql-rest.php

--- mysql-rest.php
+++ mysql-rest.php
@@ -1,5 +1,6 @@
 <?php
- 
+ //https://www.leaseweb.com/labs/2015/10/creating-a-simple-rest-api-in-php/
+
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
